a28eee54601503168c017370f7e36e9cc2b033f8
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-08-15 00:24:31 +0200

    .

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -33,13 +33,14 @@ public class Main {
                     balance();
                     break;
             }
+
+            System.out.println();
         }
 
         System.out.println("Bye!");
     }
 
     private static int menu() {
-        System.out.println();
         System.out.println("Choose your action:");
         System.out.println("1) Add income");
         System.out.println("2) Add purchase");
@@ -48,6 +49,34 @@ public class Main {
         System.out.println("0) Exit");
         int ret = Integer.parseInt(scanner.nextLine());
         System.out.println();
+
+        return ret;
+    }
+
+    private static int menuPurchase() {
+        return menuPurchase(false);
+    }
+
+    private static int menuPurchase(boolean withAll) {
+        System.out.println("\nChoose the type of purchase");
+        List<String> options = new ArrayList<>();
+        options.add("Food");
+        options.add("Clothes");
+        options.add("Entertainment");
+        options.add("Other");
+        if (withAll) options.add("All");
+        options.add("Back");
+
+        for (int i = 0; i < options.size(); i++) {
+            System.out.printf("%d) %s\n", i + 1, options.get(i));
+        }
+
+        int ret = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        if (withAll) {
+            if (!options.get(ret - 1).equals("Back")) System.out.println(options.get(ret - 1));
+        }
 
         return ret;
     }
@@ -63,32 +92,43 @@ public class Main {
     }
 
     private static void addPurchase() {
-        String[] details = new String[2];
+        int option = -1;
+        while ((option = menuPurchase()) != 5 ) {
+            String[] details = new String[3];
 
-        System.out.println("Enter purchase name:");
-        details[0] = scanner.nextLine();
+            System.out.println("Enter purchase name:");
+            details[0] = scanner.nextLine();
 
-        System.out.println("Enter its price:");
-        details[1] = scanner.nextLine();
-        money -= Double.parseDouble(details[1]);
+            System.out.println("Enter its price:");
+            details[1] = scanner.nextLine();
+            money -= Double.parseDouble(details[1]);
 
-        purchases.add(details);
-        System.out.println("Purchase was added!");
+            details[2] = Integer.toString(option);
+
+            purchases.add(details);
+            System.out.println("Purchase was added!");
+        }
     }
 
     private static void showListOfPurchases() {
-        if (purchases.size() == 0) {
-            System.out.println("Purchase list is empty");
-            return;
+        Double total;
+        int counter = 0;
+
+        int option = -1;
+        while ((option = menuPurchase(true)) != 6) {
+            total = 0.00;
+            for (String[] item : purchases) {
+                if (option != Integer.parseInt(item[2]) && option != 5) continue;
+                System.out.printf("%s $%s\n", item[0], item[1]);
+                total += Double.parseDouble(item[1]);
+                counter++;
+            }
+
+            if (counter == 0) {
+                System.out.println("Purchase list is empty!");
+            } else {
+                System.out.printf("Total sum: $%s\n", total);
+            }
         }
-
-        Double total = 0.00;
-
-        for (String[] item : purchases) {
-            System.out.printf("%s $%s\n", item[0], item[1]);
-            total += Double.parseDouble(item[1]);
-        }
-
-        System.out.printf("Total sum: $%s\n", total);
     }
 }
